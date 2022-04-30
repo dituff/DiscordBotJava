64f9886658b6001db7baaf8b2e5bd5212790fdda
dituff <alex007in@gmail.com>
2022-04-30 04:37:40 +0300

    check

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -6,7 +6,7 @@ import net.dv8tion.jda.api.JDABuilder;
 
 public class Bot {
     public static void main(String[] args) throws Exception {
-        JDA jda = JDABuilder.createDefault("OTYyMDU5MjE3ODA2NzIxMDI0.YlCBdw.Tf55PCg-7ioEn8w5IoyvOdKGO94").build();
+        JDA jda = JDABuilder.createDefault("").build();
 
         jda.addEventListener(new choiceMenu());
 
